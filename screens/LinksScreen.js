d9af02e1b7c30337938dea6ff09681d6dd4126ee
Appvantage 010 <wyne@appvantage.asia>
2019-03-08 16:54:47 +0630

    Pre

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,80 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    header: null,
   };
+
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+              <Icon
+                name="qrcode"
+                color="#c0392b"
+                size={200}
+                type="material-community"
+              />
+              <Text style={styles.name}>
+                The Legendary John Doe and his Duck
+              </Text>
+          </View>
+              <View style={styles.choose}>
+                  <Button 
+                      buttonStyle={styles.parent}
+                      titleStyle={styles.parenttitle}
+                      title="Get Started"
+                      onPress={() => console.log('Pressed')}
+                      raised={true}
+                  />
+                  <Button 
+                      style={styles.child}
+                      title="Get Started"
+                      onPress={() => console.log('Pressed')}
+                      raised={true}
+                  />
+              </View>
+              <Button 
+              title="Get Started"
+              raised={true} 
+              onPress={() => console.log('Pressed')} />
+        </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  name: {
+      paddingTop: 15,
+      fontWeight: 'bold',
+      fontSize: 20,
+      textAlign: 'center'
+  },
+  parenttitle: {
+      color: 'red'
+  },
+  choose: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 30,
+    paddingBottom: 30
+  },
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
+    paddingLeft: 20,
+    paddingRight: 20
   },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  }
 });
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,82 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+import { Button, Icon, Input, Card, ListItem } from 'react-native-elements';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    header: null,
   };
 
+  state = {
+    name: '',
+    role: '',
+    list: [
+      {
+        title: 'Study Duckopedia',
+        icon: 'book'
+      },
+      {
+        title: 'Feed the Duck',
+        icon: 'pets'
+      },
+    ]
+  };
+
+  
+
+
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <Card 
+          title="Lists-in-progress"
+          titleStyle={styles.text}
+          >
+          {
+            this.state.list.map((item, i) => (
+              <ListItem
+              topDivider
+              key={i}
+              title={item.title}
+              leftIcon={{ name: item.icon }}
+              badge={{ value: 'John Doe' }}
+              onPress={() => console.log('Pressed')}
+            />
+                  ))
+          }
+          </Card>
+          <Button 
+              title="Add a task"
+              raised={true} 
+              onPress={() => console.log('Pressed')} />
+        </ScrollView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  }
+});
